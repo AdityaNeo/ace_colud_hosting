de7e5ebead8a2aefd3b3a8e32bdb516eb7c6f8f2
Aditya <aditya.lenka@neosoftmail.com>
2022-03-04 23:17:19 +0530

    all responsive design complete only crad slider pending

--- app/js/header.js
+++ app/js/header.js
@@ -1,42 +1,45 @@
 const hamburger=document.querySelector(".header__hamburger");
 const topnav=document.querySelector(".header__top-links-wrapper");
-const bottomnav=document.querySelector(".header__nav-wrapper");
+const bottomnav=document.querySelector(".header__nav-links");
 
 const toplink=document.querySelector(".header__top-links");
 const bottomlink=document.querySelector(".header__nav-links-wrapper");
 
 
-const all_li=document.querySelectorAll("ul li");
-const all_anchor=document.querySelectorAll("ul>li a");
+const all_anchor=document.querySelectorAll(".header__nav-links > a");
+
+
+// const all_anchor=document.querySelectorAll("ul>li a");
 
    hamburger.addEventListener("click",()=> { 
 
    hamburger.classList.toggle("active");
    topnav.classList.toggle("active");
    bottomnav.classList.toggle("active");
+   // header_wrap.classList.toggle("header__wrapper-remove-margin");
 
-  for(let ele of toplink.children)
+//   for(let ele of toplink.children)
+//   {
+//     ele.classList.add("nav-item");
+//   }
+
+
+//   for(let ele of bottomlink.children)
+//   {
+//     ele.classList.add("nav-item");
+//   }
+
+
+  for(let margin of all_anchor)
   {
-    ele.classList.add("nav-item");
+     margin.classList.add("remove-margin");
+
   }
-
-
-  for(let ele of bottomlink.children)
-  {
-    ele.classList.add("nav-item");
-  }
-
-
-  for(let border of all_li)
-  {
-     border.classList.add("remove-border");
-
-  }
-  for(let border of all_anchor)
-  {
-     border.classList.add("remove-border");
+//   for(let border of all_anchor)
+//   {
+//      border.classList.add("remove-border");
      
-  }
+//   }
   
 
 })
